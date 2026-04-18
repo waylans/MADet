0cb11dffea946db2494682a4d409668e770d877f
wnorth <136388058@qq.com>
2026-04-18 23:01:35 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1746,7 +1746,7 @@ def parse_model(d, ch, verbose=True):
             args.extend([reg_max, end2end, [ch[x] for x in f]])
             if m is Segment or m is YOLOESegment or m is Segment26 or m is YOLOESegment26:
                 args[2] = make_divisible(min(args[2], max_channels) * width, 8)
-            if m in {Detect, Detect_SEAM, YOLOEDetect, Segment, Segment26, YOLOESegment, YOLOESegment26, Pose, Pose26, OBB, OBB26}:
+            if m in {Detect, YOLOEDetect, Segment, Segment26, YOLOESegment, YOLOESegment26, Pose, Pose26, OBB, OBB26}:
                 m.legacy = legacy
 
         
@@ -1771,44 +1771,15 @@ def parse_model(d, ch, verbose=True):
         elif m is ContrastDrivenFeatureAggregation:
             c2 = ch[f]
             args = [c2, *args]
-        # elif m is Detect_SEAM:
-        #     args = args
-        
-        #     try:
-        #         if isinstance(f, (list, tuple)) and len(f):
-        #             c2 = ch[f[0]]
-        #         else:
-        #             c2 = ch[f] if isinstance(f, int) else ch[-1]
-        #     except Exception:
-        #         c2 = ch[-1]
-        
 
-        elif m in frozenset({Detect,Detect_SEAM,WorldDetect,
-                YOLOEDetect,Segment,Segment26,YOLOESegment,
-                YOLOESegment26,Pose,Pose26,OBB,OBB26,}):
-            args.extend([reg_max, end2end, [ch[x] for x in f]])
-
-            if m in {Segment, Segment26, YOLOESegment, YOLOESegment26}:
-                args[2] = make_divisible(min(args[2], max_channels) * width, 8)
-
-            if m in {
-                Detect,
-                Detect_SEAM,
-                YOLOEDetect,
-                Segment,
-                Segment26,
-                YOLOESegment,
-                YOLOESegment26,
-                Pose,
-                Pose26,
-                OBB,
-                OBB26,}:
-                m.legacy = legacy
-
+        elif m is Detect_SEAM:
+            # 不要复用 Detect 的 args.extend([reg_max, end2end, [ch[x] for x in f]])
+            # 按 Detect_SEAM.__init__ 实际签名来组装 args
             if isinstance(f, (list, tuple)) and len(f):
                 c2 = ch[f[0]]
             else:
                 c2 = ch[f] if isinstance(f, int) else ch[-1]
+        
 
         else:
             c2 = ch[f]
